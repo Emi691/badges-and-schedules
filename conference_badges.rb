284d34eb4d9b9caaedd8b96b542e4352f6f105b6
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-12 18:32:47 -0800

    commit tree 813146e9dd9e36355a977a4bf0576c4fd9b99ab5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker (name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+    array.map do |name|
+    "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms (array)
+    room_assignments = []
+    array.each_with_index do |name, room|
+       room_assignments << "Hello, #{name}! You'll be assigned to room #{room + 1}!"
+    end
+       room_assignments
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |name|
+    puts name
+    end
+    assign_rooms(array).each do |name| 
+    puts name
+    end
+end
